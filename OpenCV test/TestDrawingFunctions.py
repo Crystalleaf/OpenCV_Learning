3d602a94393f2d97aedca4c9b8917e440a664da5
Administrator <Administrator@HY50-20160516OB>
2017-03-10 11:39:23 +0800

    add mouse as Paint Brush function

---
+++ OpenCV test/TestDrawingFunctions.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2
+
+# Create a black image
+img = np.zeros((512,512,3), np.uint8)
+
+# Draw a diagonal blue line with thickness of 5 px
+cv2.line(img, (0,0), (511,511), (255,0,0), 5)
+
+# Draw rectangle (color is BGR)
+cv2.rectangle(img, (384,0), (510,128), (0,255,0), 3)
+
+# Draw circle (thickness = -1 will fill the shape)
+cv2.circle(img, (447,63), 63, (0,0,255), -1)
+
+# Draw ellipse
+cv2.ellipse(img, (256,256), (100,50), 0, 0, 180, 255, -1)
+
+# Draw polygon
+pts = np.array([[10,5], [20,30], [70,20], [50,10]], np.int32)
+pts = pts.reshape((-1,1,2))
+cv2.polylines(img, [pts], True, (0,255,255), 3)
+
+# Add text
+font = cv2.FONT_HERSHEY_COMPLEX
+cv2.putText(img, "OpenCV", (5,480), font, 4, (255,255,255), 2, cv2.LINE_AA)
+
+cv2.imshow("Image", img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
